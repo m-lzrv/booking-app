1066ae081a9d9b85b5272c0aac9f9512c7098340
max <maxlazareffsky@gmail.com>
2022-03-30 23:44:30 +0400

    lil fixes

--- src/main/kotlin/com/booking/bookingapp/configs/AppConfig.kt
+++ src/main/kotlin/com/booking/bookingapp/configs/AppConfig.kt
@@ -1,14 +1,11 @@
 package com.booking.bookingapp.configs
 
-import com.booking.bookingapp.db.repositories.DeviceBookingRepository
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 
 @Configuration
-@EnableJpaRepositories(basePackageClasses = [DeviceBookingRepository::class])
 @EnableConfigurationProperties(AvailablePhonesProperties::class)
 @EnableTransactionManagement
 class AppConfig
